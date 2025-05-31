4238762f6a52ab2c35aca85f2bf4c9cd86d46ba3
Aryan Dumyan <aryandumyan@Aryans-MacBook-Air.local>
2025-05-31 11:55:39 +0530

    vairables and datatypes

---
+++ Variable2.java
@@ -0,0 +1,25 @@
+//java is strongly typed language.
+
+class Variable2 {
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println(number);
+
+        float marks = 6.5f;
+        double d = 6.5;
+        byte b = 45;
+        short sh = 5;
+
+        long l = 2412l;
+        boolean t = true;
+    }
+}
+
+//inbuilt datatypes are primitive(integer, float, char, bool)
+// int -> 4 bytes, long -> 8 bytes, short->2 bytes, byte = 1 byte(-2^7 to 2^7+1)
+//float -> 4 bytes, double -> 8 bytes , double is default
+
+//as by default double so float = 5.6 will give us error. we have to specify like float = 5.6f
+
+//char-> 2 bytes, char c = 'k' , double quotes -> string
+//boolean -> true or false, no 0,1
