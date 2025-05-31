bc52c28c0750e18c5cf41542688b77814044634d
Aryan Dumyan <aryandumyan@Aryans-MacBook-Air.local>
2025-05-31 12:02:35 +0530

    literals

---
+++ literal3.java
@@ -0,0 +1,18 @@
+class literal {
+    public static void main(String a[]){
+        int num = 0b101; //binary number 101
+        int hex = 0x7E;
+        int num1 = 10_00_00_000;
+        //these undersocre help to check number of zeros for programmers
+        System.out.println(num1);
+        System.out.println(hex);
+        System.out.println(num);
+
+        double d = 56; //integer will convert to double means 56.0
+        boolean b = 1; //int can not be converted ot boolean
+
+        char c = 'a';
+        a++; //-> b
+        
+    }
+}
