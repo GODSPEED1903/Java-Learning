65f1f1bb35d6e8687bfd4bc8b6a791d5d886bbce
Aryan Dumyan <aryandumyan@Aryans-MacBook-Air.local>
2025-06-30 23:52:51 +0530

    tools

---
+++ seventeen/Demo.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+import tools.calc;
+
+//like this we import packages. System.out is also imported from java.lang.* , * means import all files from that package
+public class Demo{
+    public static void main(String a[]){
+        ArrayList arr = new ArrayList();
+    }
+    calc obj = new calc();
+}
